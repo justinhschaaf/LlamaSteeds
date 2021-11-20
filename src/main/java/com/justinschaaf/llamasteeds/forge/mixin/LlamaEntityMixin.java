36669b1ae61711f48f114215b60dea4673d7f22c
Justin Schaaf <justinhschaaf@outlook.com>
2021-11-20 11:43:49 -0800

    Update to 1.17.1
    - Updated to use Java 16
    - Supports Minecraft 1.17.1+
    - Supports Minecraft Forge 37.0.59+
    - Switched to official mappings
    - Bump version to 2021.11.20
    - Includes proper mixin configuration in build.gradle and now generates a refmap, thanks to https://github.com/SpongePowered/Mixin/wiki/Mixins-on-Minecraft-Forge
    - Changed mods.toml line ending from \r\n to \n to fix strange character in extended description

--- src/main/java/com/justinschaaf/llamasteeds/forge/mixin/LlamaEntityMixin.java
+++ src/main/java/com/justinschaaf/llamasteeds/forge/mixin/LlamaEntityMixin.java
@@ -1,21 +1,21 @@
 package com.justinschaaf.llamasteeds.forge.mixin;
 
-import net.minecraft.entity.passive.horse.LlamaEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import net.minecraft.world.entity.animal.horse.Llama;
 
-@Mixin(LlamaEntity.class)
+@Mixin(Llama.class)
 public class LlamaEntityMixin {
 
     // Lets llamas be saddled
     @Overwrite
-    public boolean func_230264_L__() {
+    public boolean isSaddleable() {
         return true;
     }
 
     // Lets llamas be steered when saddled
     @Overwrite
-    public boolean canBeSteered() {
+    public boolean canBeControlledByRider() {
         return true;
     }
 
