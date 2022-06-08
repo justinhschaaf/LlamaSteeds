6a3571b9e2eacc611f16fa3f3d675a01b5c22b1d
Justin Schaaf <justinhschaaf@outlook.com>
2022-06-07 23:43:55 -0700

    The wILD update - Fabric
    - Updated version to 2022.06.08
    - Updated to Java 17
    - Updated Minecraft version to 1.19
    - Updated to work with Minecraft 1.19
    - Updated Yarn mappings
    - Updated Fabric Loader
    - Updated Gradle
    - Updated Fabric Loom
    - Updated License Years
    - Updated README.md with a new screenshot and linking Curseforge in the badges
    - Updated the list of things updated to look longer and more impressive
    - Cleaned up build.gradle to match updated fabric template--and to actually work again

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 ![Icon](https://github.com/justinhschaaf/LlamaSteeds/raw/master/docs/ICON_TEXT.png)
 
-![Downloads](https://cf.way2muchnoise.eu/llama-steeds.svg?badge_style=for_the_badge)
-![Versions](https://cf.way2muchnoise.eu/versions/llama-steeds.svg?badge_style=for_the_badge)
+[![Downloads](https://cf.way2muchnoise.eu/llama-steeds.svg?badge_style=for_the_badge)
+![Versions](https://cf.way2muchnoise.eu/versions/llama-steeds.svg?badge_style=for_the_badge)](https://www.curseforge.com/minecraft/mc-mods/llama-steeds)
 
 *Mount your llama and ride into the sunset.*
 
 Have you dreampt of riding a llama? Well now you can! With this astonishingly simple mod, you can saddle your llama and ride it as if it were a horse.
 
-### Now with more nether! (1.16 update)
+### Now with more nature! (1.19 update)
 
-![Nether Llama](https://github.com/justinhschaaf/LlamaSteeds/raw/master/docs/NETHER.png)
+![Wild Llama](https://github.com/justinhschaaf/LlamaSteeds/raw/master/docs/WILD.png)
 
 ## Ride a Llama in 3 Easy Steps:
 
--- src/main/java/com/justinschaaf/llamasteeds/LlamaSteeds.java
+++ src/main/java/com/justinschaaf/llamasteeds/LlamaSteeds.java
@@ -1,5 +1,7 @@
 package com.justinschaaf.llamasteeds;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.passive.LlamaEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
@@ -10,7 +12,7 @@ public class LlamaSteeds {
     /**
      * I mount my steed...
      *
-     * @note Stupid stuff required by Gradle to build the jar. WHY?
+     * @note Gradle wants docs before it builds the jar for...some reason?
      * @return Whether or not the llama can be saddled
      * @reason So llamas can be controlled
      * @author justinhschaaf
@@ -23,14 +25,33 @@ public class LlamaSteeds {
     /**
      * ...and I set forth on my journey
      *
-     * @note Stupid stuff required by Gradle to build the jar. WHY?
-     * @return Whether or not the llama can be controlled by the rider
-     * @reason So llamas can be controlled
+     * @note The "canBeControlledByRider" method was removed in 1.19. This
+     *      replaces it going forward.
+     * @return The llama's rider. This is overriden by the LlamaEntity class to
+     *      return null, I'm guessing so there is no primary passenger to
+     *      control the llama. Not anymore!
      * @author justinhschaaf
      */
     @Overwrite
-    public boolean canBeControlledByRider() {
-        return true;
+    public LivingEntity getPrimaryPassenger() {
+
+        // It would have been so much more convenient if this worked
+        // --but no, since you can't, I will write out my own version as you wish, Mixin Gods
+        //return ((AbstractDonkeyEntity) (Object) super).getPrimaryPassenger();
+
+        LlamaEntity llama = (LlamaEntity) (Object) this;
+
+        if (llama.isSaddled()) {
+
+            Entity rider = llama.getFirstPassenger();
+
+            if (rider instanceof LivingEntity)
+                return (LivingEntity) rider;
+
+        }
+
+        return null;
+
     }
 
 }
