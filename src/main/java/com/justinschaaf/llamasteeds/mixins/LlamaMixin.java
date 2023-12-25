509b3dc276bee5201fd2733c8523ab451d708eca
Justin Schaaf <justinhschaaf@outlook.com>
2023-12-25 01:08:12 -0800

    Finally Fix Fabric
    - Bumped version to 2023.12.25
    - Updated Minecraft to 1.20.2. Confirmed that this also works on 1.20.4
    - Updated Gradle
    - Updated Fabric Loader to 1.15.3
    - Updated Fabric API to 0.91.2
    - Updated Fabric Loom to 1.4-SNAPSHOT

--- src/main/java/com/justinschaaf/llamasteeds/mixins/LlamaMixin.java
+++ src/main/java/com/justinschaaf/llamasteeds/mixins/LlamaMixin.java
@@ -1,7 +1,5 @@
 package com.justinschaaf.llamasteeds.mixins;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.passive.LlamaEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
@@ -22,37 +20,12 @@ public class LlamaMixin {
         return true;
     }
 
-    /**
+    /*
      * ...and I set forth on my journey
      *
-     * @note The "canBeControlledByRider" method was removed in 1.19. This
-     *      replaces it going forward.
-     * @return The llama's rider. This is overriden by the LlamaEntity class to
-     *      return null, I'm guessing so there is no primary passenger to
-     *      control the llama. Not anymore!
-     * @reason So llamas can be controlled
-     * @author justinhschaaf
+     * There was "getControllingPassenger", then "canBeControlledByRider" before
+     * 1.19. As of 1.20.2, it's not overriden by the Llama class anymore and is
+     * no longer needed for our use.
      */
-    @Overwrite
-    public LivingEntity getControllingPassenger() {
-
-        // It would have been so much more convenient if this worked
-        // --but no, since you can't, I will write out my own version as you wish, Mixin Gods
-        //return ((AbstractDonkeyEntity) (Object) super).getPrimaryPassenger();
-
-        LlamaEntity llama = (LlamaEntity) (Object) this;
-
-        if (llama.isSaddled()) {
-
-            Entity rider = llama.getFirstPassenger();
-
-            if (rider instanceof LivingEntity)
-                return (LivingEntity) rider;
-
-        }
-
-        return null;
-
-    }
 
 }
